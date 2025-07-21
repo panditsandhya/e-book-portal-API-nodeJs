5ad11b2e55141ee4b56b3b557172b02827e6c774
Sandhya110 <starsandhya369@gmail.com>
2025-07-21 19:45:15 +0530

    add databse setup

--- server.ts
+++ server.ts
@@ -1,12 +1,16 @@
-
 import app from "./src/app";
+import { config } from "./src/config/config";
+import connectDB from "./src/config/db";
 
-const startServer = () => {
-  const port = process.env.PORT || 3000;
+const startServer = async () => {
+
+  //connect database
+  await connectDB();
+
+  const port = config.port || 3000;
 
   app.listen(port, () => {
     console.log(`Listening on port: ${port}`);
-    
   });
 };
 
---
+++ src/config/config.ts
@@ -0,0 +1,10 @@
+import { config as conf } from "dotenv";
+
+conf();
+
+const _config = {
+  port: process.env.PORT,
+  databaseUrl: process.env.MONGODB_URL
+};
+
+export const config = Object.freeze(_config);
