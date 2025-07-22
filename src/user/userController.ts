199745900c029d19bb3fb423290a1b7513451104
Sandhya110 <starsandhya369@gmail.com>
2025-07-22 13:50:27 +0530

    add error handling

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -5,6 +5,7 @@ import userModel from "./userModel";
 import bcrypt from "bcrypt";
 import { sign } from "jsonwebtoken";
 import { config } from "../config/config";
+import { User } from "./userTypes";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   const { name, email, password } = req.body;
@@ -16,32 +17,45 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
   }
 
   // Database call
+  try {
+    const user = await userModel.findOne({ email });
 
-  const user = await userModel.findOne({ email });
-
-  if (user) {
-    const error = createHttpError(400, "User already exists with this email.");
-    return next(error);
+    if (user) {
+      const error = createHttpError(
+        400,
+        "User already exists with this email."
+      );
+      return next(error);
+    }
+  } catch (error) {
+    return next(createHttpError(500, "Error while getting user"));
   }
 
   //password hash
   const hashedPassword = await bcrypt.hash(password, 10);
 
-  const newUser = await userModel.create({
-    name,
-    email,
-    password: hashedPassword,
-  });
+  let newUser: User;
+
+  try {
+    newUser = await userModel.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+  } catch (error) {
+    return next(createHttpError(500, "Error while creating user."));
+  }
 
   //Token generation - JWT
-  const token = sign({ sub: newUser._id }, config.jwtSecret as string, {expiresIn: '7d'});
- //Response
-  res.json({ accessToken: token });
-
-  // process
-  // response
-
-  res.json({ id: newUser._id });
+  try {
+    const token = sign({ sub: newUser._id }, config.jwtSecret as string, {
+      expiresIn: "7d",
+    });
+    //Response
+    res.json({ accessToken: token });
+  } catch (error) {
+    return next(createHttpError(500, "Error while signing the jwt token"));
+  }
 };
 
 export { createUser };
