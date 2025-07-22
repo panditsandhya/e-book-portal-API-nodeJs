b23353adf57a84b0e1a49b7ca73963995e259b00
Sandhya110 <starsandhya369@gmail.com>
2025-07-22 14:05:10 +0530

    change create user status code

--- src/user/userRouter.ts
+++ src/user/userRouter.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import { createUser } from "./userController";
+import { createUser, loginuser } from "./userController";
 
 const userRouter = express.Router()
 
 //routes
 userRouter.post('/register',createUser);
+userRouter.post('/login', loginuser);
 
 
 export default userRouter;
