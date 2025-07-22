152c563d154e4218f8d28f319924cd075b67cad7
Sandhya110 <starsandhya369@gmail.com>
2025-07-22 15:19:06 +0530

    add login endpoint

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -58,6 +58,30 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
   }
 };
 
+const loginuser = async (req: Request, res: Response, next: NextFunction) => {
+  const { email, password } = req.body;
 
+  if (!email || !password) {
+    return next(createHttpError(400, "All fields are required"));
+  }
 
-export { createUser};
+  const user = await userModel.findOne({ email });
+  if (!user) {
+    return next(createHttpError(404, "User not found"));
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    return next(createHttpError(400, "Username or password incorrect!"));
+  }
+   
+  //todo: handle errors
+  // create accessToken
+  const token = sign({ sub: user._id }, config.jwtSecret as string, {
+    expiresIn: "7d",
+  });
+
+  res.json({ accessToken: token });
+};
+
+export { createUser, loginuser };
